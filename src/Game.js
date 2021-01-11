7adf1bdc234dc59f4ef668b632638944afed7615
CoolHyung <hyungjoolee131@gmail.com>
2021-01-11 23:15:08 +0000

    added config component

---
+++ src/Game.js
@@ -0,0 +1,16 @@
+
+
+function Config(){
+	return (
+      <form>
+        <label htmlFor="fname">How Many Players</label>
+        <br />
+        <input type="text" id="fname" name="fname" />
+        <br />
+      </form>
+
+		);
+
+}
+
+export default Config;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Config from './Board.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Config />
   </React.StrictMode>,
   document.getElementById('root')
 );
